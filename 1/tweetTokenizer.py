87a0c0ab699009c827e56640a7f4217eeb88fbbc
Prabhakar Bikkaneti <prabhakar.9885@gmail.com>
2016-08-11 16:12:59 +0530

    Bug Fix: unable to tokenize the unicode emojis

--- 1/tweetTokenizer.py
+++ 1/tweetTokenizer.py
@@ -1,19 +1,28 @@
+# -*- coding: utf-8 -*-
+
 from string import punctuation
 import re, itertools
 
-smileys = """:D :-D :) :-) :-E >-) :( :-( :-< :P :-O :-* :-@ :'( :-$ :-\ :-# (((H))) :-X `:-) :^) :-& E-:-) <:-) :-> (-}{-) :-Q $_$ @@ :-! :-D :*) :@ :-@ :-0 :-----) %-( :-.) :-($) (:I     |-O :@) <(-_-)> d[-_-]b ~:0 -@--@- \VVV/ \%%%/ :-# :'-) {:-) ;) ;-) O:-) O*-) |-O (:-D @>--;-- @-}--- =^.^= O.o \_/) (o.o) (___)0 ~( 8^(I)"""
+smileys = """<3 :D :-D :) :-) :P :-P :-E >-) :( :-( :-< :P :-O :-* :-@ :'( :-$ :-\ :-# (((H))) :-X `:-) :^) :-& E-:-) <:-) :-> (-}{-) :-Q $_$ @@ :-! :-D :*) :@ :-@ :-0 :-----) %-( :-.) :-($) (:I     |-O :@) <(-_-)> d[-_-]b ~:0 -@--@- \VVV/ \%%%/ :-# :'-) {:-) ;) ;-) O:-) O*-) |-O (:-D @>--;-- @-}--- =^.^= O.o \_/) (o.o) (___)0 ~( 8^(I)"""
 
 regex = {}
 regex['word'] = re.compile( "([A-Za-z0-9\-_]+)" );
 regex['smileys'] = re.compile( "|".join( map(re.escape,smileys.split()) ) )
+regex['unicodeEmoji'] = re.compile(u'(['
+								    u'\U0001F300-\U0001F64F'
+								    u'\U0001F680-\U0001F6FF'
+								    u'\u2600-\u26FF\u2700-\u27BF])', 
+								    re.UNICODE)
 regex['mentions'] = re.compile( r"(@\w+)" )
 regex['tags'] = re.compile( r"(#[A-Za-z0-9]+)" )
 regex['email'] = re.compile( r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)" )
 regex['url'] = re.compile( "(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)" )
 regex['ellipses'] = re.compile( r"...$" )
 regex['punct'] = re.compile( "|".join( [re.escape(c) for c in punctuation] ) )
-regexLst = [ regex['email'], regex['url'], regex['word'], regex['smileys'], 
-				regex['mentions'], regex['tags'], regex['ellipses'], regex['punct'] ]
+regexLst = [ regex['email'], regex['url'], regex['word'], 
+				regex['smileys'], regex['unicodeEmoji'],
+				regex['mentions'], regex['tags'], regex['ellipses'], 
+				regex['punct'] ]
 
 
 class Tokenizer(object):
