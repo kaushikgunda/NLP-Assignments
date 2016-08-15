2b3fc791ca2ddbe51f48013bf65921f66e539345
Prabhakar Bikkaneti <prabhakar.9885@gmail.com>
2016-08-15 13:21:19 +0530

    Added examples

--- 1/driver.py
+++ 1/driver.py
@@ -3,6 +3,10 @@
 import tweetTokenizer as t
 o = t.Tokenizer()
 s = "Whats' should'ntI do, Boom-boom be happy with lies or be sad with the truth 😔😩🤔"
+s = "What should I do, be happy with lies or be s😔d with the truth 😔😩🤔"
+s = "This is@a_tweet@for12Testing#the#tokens#testing:'(with :)with:D#happy#face:PLOL"
+s = "This is@a_tweet@for12Testing#the#tokens#testing:'(with :)with:D#happy#face:PLOL http://go.co.in/asdaaSD23/43sedf_sad asfda@sdgs.v.in"
+s = "Fuck:(!!!!!"
 print s
 s = s.decode('utf8')
 print s
